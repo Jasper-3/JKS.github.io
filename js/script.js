8bd37e94a98170749f5fec01c27d580e5b78cb56
Jasper-3 <118008174+Jasper-3@users.noreply.github.com>
2022-12-01 22:09:01 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEL.textContent = currentYear;
+
+// mobile nav
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
